97166ef525ec478c6ee51833df5ad36ed7933253
Kyle Jensen <kyle.jensen@bynd.com>
2016-06-16 09:30:11 -0400

    Adds battleship file

---
+++ battleship.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+from random import randint
+
+"""
+    Simple Battleship Game, 4 turns, board size is 5x5
+"""
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print "Let's play Battleship!"
+print "Choose a row and column on this 5x5 board"
+print "It is zero-indexed"
+print "Try to find the hidden battleship"
+
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+       print "Congratulations! You sunk my battleship!"
+       break;
+    else:
+      if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+           print "Oops, that's not on the board."
+      elif(board[guess_row][guess_col] == "X"):
+          print "You guessed that one already."
+      else:
+          print "You missed my battleship!"
+          board[guess_row][guess_col] = "X"
+    if turn == 3:
+        print "Game Over"
+        print "My battleship was at row %s col %s" % (ship_row, ship_col)
+
+    if turn < 3:
+        print "Turn", turn + 1
